558698d15d8dc4f9f7766228c2db084954f4ec02
jiqinghua <jiqinghua@souche.com>
2016-07-12 14:45:25 +0800

    init commit

---
+++ template/examples/App.vue
@@ -0,0 +1,19 @@
+<style lang="less">
+
+</style>
+
+<template>
+    <div id="app">
+        <test-component></test-component>
+    </div>
+</template>
+
+<script>
+import TestComponent from '../src/index.vue';
+
+export default {
+    components: {
+        TestComponent
+    }
+};
+</script>
---
+++ template/src/index.vue
@@ -0,0 +1,12 @@
+<style lang="less">
+</style>
+
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: ''
+};
+</script>
